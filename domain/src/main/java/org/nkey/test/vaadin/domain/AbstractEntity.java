7f0f82155ccda72a0fcf979ea0a2d6dc5be03e1b
Michail Nikolaev <m.nikolaev@itransition.com>
2012-10-30 16:54:45 +0300

    id fix

--- domain/src/main/java/org/nkey/test/vaadin/domain/AbstractEntity.java
+++ domain/src/main/java/org/nkey/test/vaadin/domain/AbstractEntity.java
@@ -4,17 +4,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
-import javax.persistence.SequenceGenerator;
 
 /**
  * @author m.nikolaev Date: 30.10.12 Time: 22:44
  */
 @MappedSuperclass
 public class AbstractEntity<T extends AbstractEntity> {
-    private static final String SEQUENCE_NAME = "id_seq";
 
-    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = SEQUENCE_NAME) @SequenceGenerator(
-            name = SEQUENCE_NAME, sequenceName = SEQUENCE_NAME)
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     public Long getId() {
